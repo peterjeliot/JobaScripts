95c57d3b5226d3365ff223c302675e6956660eef
Joseph Combs <joseph.e.combs@gmail.com>
2015-01-04 19:46:46 -0800

    pointing back at the mothership in a sec...

--- linkedin.rb
+++ linkedin.rb
@@ -14,14 +14,22 @@ def write_to_google_doc(jobs_hash)
   puts "writing all the information into a google spreadsheet"
 end
 
+def purify_job(job_hash)
+  #take all information about the job, then select only the attributes we want and return a simplified hash
+  #simplified_hash
+  job_hash
+end
+
 a = Mechanize.new
 a.user_agent_alias= 'Mac Safari'
 
+cleaned_jobs = []
 
 a.get('https://www.linkedin.com/') do |page|
 # a.get('http://www.josephecombs.com/') do |page|
-
+  
   # login to linkedin
+  
   login_page = a.get("https://www.linkedin.com")
 
   splash_page = login_page.form_with(action: 'https://www.linkedin.com/uas/login-submit') do |f|
@@ -39,10 +47,10 @@ a.get('https://www.linkedin.com/') do |page|
   
   #be kind to the folks at linkedin and only look at 4 pages of job results
   while all_jobs_posted_today && pages_scraped < 4 do
-    #voltron = jobs_page.search("#voltron_srp_main-content")
-    doc = Nokogiri::HTML(open('voltron.html'))
-    voltron = doc.css('#voltron_srp_main-content').to_s
-  
+    voltron = jobs_page.search("#voltron_srp_main-content")
+    # doc = Nokogiri::HTML(open('voltron.html'))
+    # voltron = doc.css('#voltron_srp_main-content').to_s
+
     # fuck it, we'll do it live
     start_point = voltron.index("<!--{") + 4
     end_point = voltron.index("}-->")
@@ -60,6 +68,8 @@ a.get('https://www.linkedin.com/') do |page|
       if Date.parse(element['job']['fmt_postedDate']) == Date.today
       # if (0..99).to_a.sample > 5
         puts "writing job to jobs hash"
+        #cleaned_jobs.push(purify_job(element['job'])
+        cleaned_jobs.push(element['job'])
         #for testing, find the terminating job listing with 5% probability
         #write only the job information you need into some hash, then dump that into a google doc
       else
@@ -70,6 +80,8 @@ a.get('https://www.linkedin.com/') do |page|
     if all_jobs_posted_today
       jobs_page = jobs_page.link_with(title: "Next Page").click
       pages_scraped += 1
+      puts "sleeping to not cause linkedin to ban me"
+      sleep 16
     end
   end
   
@@ -83,3 +95,4 @@ a.get('https://www.linkedin.com/') do |page|
   #end
 end
 
+puts "logged this many jobs into jobs hash: " + cleaned_jobs.length
